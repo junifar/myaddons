26784e5655bf0de7566fe05c59abb26cc93aed32
junifar <junifar@gmail.com>
2017-03-22 16:19:38 +0700

    Adding Device List to create connection finger print
    - Get user Name

--- prasetia-hr/models/models.py
+++ prasetia-hr/models/models.py
@@ -65,6 +65,7 @@ class DeviceAttendance(models.Model):
             conn = zk.connect()
             users = conn.get_users()
             self.device_attendance_user_ids = [{'name': 'junifar', 'user_id': 1}]
+            print('jalan kok')
             for user in users:
                 pass
                 # print ('name : %s - user_id = %d' % (user.name, user.uid))
