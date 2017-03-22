6fb57d1408da5a1aa5162ea94912722abdf6836e
junifar <junifar@gmail.com>
2017-03-22 10:19:51 +0700

    Adding Device List to create connection finger print
    - improvement

--- prasetia-hr/models/models.py
+++ prasetia-hr/models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions, _
+from zk import ZK
 
 
 class EmployeeFamily(models.Model):
@@ -52,3 +52,24 @@ class DeviceAttendance(models.Model):
     device_attendance_type_id = fields.Many2one('device.attendance.type', string='Device Type')
     ip_address = fields.Char(string='IP Address')
     port = fields.Char(string="Port")
+    device_attendance_user_ids = fields.One2many('device.attendance.user', 'device_attendance_id',
+                                                 string="List User In Fingerprint Device")
+
+    @api.multi
+    def import_user(self):
+        conn = None
+        zk = ZK(self.ip_address, port=int(self.port), timeout=5)
+        try:
+            zk.connect()
+        except Exception as e:
+            raise exceptions.except_orm(_('Error'), _('Can\'t connect to device, IP : %s port %s : {}'.format(e) % (self.ip_address, self.port)))
+        raise exceptions.except_orm(_('Success'), _('Connection Established'))
+        return {}
+
+
+class DeviceAttendanceImportUser(models.Model):
+    _name = "device.attendance.user"
+
+    device_attendance_id = fields.Many2one('device.attendance', required=True, string="Device Name")
+    name = fields.Char(required=True, string='User Name')
+    user_id = fields.Integer(string='User ID/UID')
