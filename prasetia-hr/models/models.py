d0b5e5a3ac30406a8c645417d1900e0e8f87fd1d
junifar <junifar@gmail.com>
2017-03-23 15:08:39 +0700

    Import Absen From Devices
    Adding Import Button

--- prasetia-hr/models/models.py
+++ prasetia-hr/models/models.py
@@ -96,12 +96,12 @@ class AttendanceImport(models.Model):
 
             attendances = conn.get_attendance()
             for attendance in attendances:
+                print attendance.timestamp.date() + ' - ' + self.name.name
                 if attendance.timestamp.date() == self.name:
-                    print attendance.timestamp.date()
                     self.attendance_import_line_ids = [{'name': 2609,
                                                         'attendance_import_id': self.id,
                                                         'absent': attendance.timestamp}]
-                # print attendance.timestamp.date()
+                    # print attendance.timestamp.date()
         except Exception as e:
             raise exceptions.except_orm(_('Error'), _(
                 'Can\'t connect to device, IP : %s port %s : {}'.format(e) % (self.device_attendance_id.ip_address,
