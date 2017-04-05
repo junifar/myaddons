8063e8f00c6f967fe4ad4bf5e8a359e69ed27a65
junifar <junifar@gmail.com>
2017-04-05 09:57:48 +0700

    Adding project Custom Module

---
+++ project_custom/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Project Improvemenr",
+
+    'summary': """
+        Customization existing official module project ODOO
+        improve dashboard interface
+        """,
+
+    'description': """
+        Project Improvement
+    """,
+
+    'author': "RubahApi",
+    'website': "http://www.junifar.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Project',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'project'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'auto_install': False,
+}
