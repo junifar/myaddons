3d80d76653014e0c797d643142d3e6dead35cee4
junifar <junifar@gmail.com>
2017-07-25 14:43:10 +0700

    Report Absen Batch

--- prasetia_hr/report/report_absen.py
+++ prasetia_hr/report/report_absen.py
@@ -4,23 +4,19 @@ from odoo import api, models
 
 
 class ReportAbsen(models.AbstractModel):
-    _name = 'report.report_absen'
+    _name = 'report.prasetia_hr.report_absen'
 
-    @api.multi
-    def render_html(self, data):
+    @api.model
+    def render_html(self, docids, data=None):
         self.model = self.env.context.get('active_model')
-        docs = self.env[self.model].browse(self.env.context.get('active_ids', []))
+        docs = self.env[self.model].browse(self.env.context.get('active_id'))
 
         docargs = {
             'doc_ids': self.ids,
             'doc_model': self.model,
             'data': data['form'],
             'docs': docs,
-            'report_type': data.get('form').get('report_type'),
-            'date_filter': data.get('form').get('date_filter'),
-            'bulan_filter': data.get('form').get('month_filter'),
-            'tahun_filter': data.get('form').get('year_filter'),
-            'transactions': self._get_atk_list(data.get('form'))
+            'report_type': docs.report_type
         }
 
-        return self.env['report'].render('report_absen', docargs)
+        return self.env['report'].render('prasetia_hr.report_absen', docargs)
--- prasetia_hr/wizard/report_absen_wizard.py
+++ prasetia_hr/wizard/report_absen_wizard.py
@@ -8,6 +8,8 @@ class report_absen_wizard(models.TransientModel):
     _description = 'Report Absent Priodicaly'
 
     company_id = fields.Many2one('res.company', required=True)
+    report_type = fields.Selection(selection=[('Harian', 'Harian'), ('Bulanan', 'Bulanan'), ('Custom', 'Custom')],
+                                   required=True, string="Report Type", default="Harian")
     month_filter = fields.Selection(selection=[
         (1, 'Januari'),
         (2, 'Februari'),
@@ -21,9 +23,28 @@ class report_absen_wizard(models.TransientModel):
         (10, 'Oktober'),
         (11, 'November'),
         (12, 'Desember')
-    ], string="Bulan", required=True)
-    year_filter = fields.Integer(string="Tahun", default=datetime.now().year, required=True)
+    ], string="Bulan")
+    year_filter = fields.Integer(string="Tahun", default=datetime.now().year)
+    date_filter = fields.Date(string="Tanggal")
+    date_filter_from = fields.Date(string="Dari Tanggal")
+    date_filter_to = fields.Date(string="Hingga Tanggal")
+
+    def _print_data(self, data):
+        return self.env['report'].with_context(landscape=False).get_action(self,
+                                                                           'prasetia_hr.report_absen',
+                                                                           data=data)
 
     @api.multi
     def show_data(self):
-        return None
+        self.ensure_one()
+        self_obj = self.env.context
+
+        data = {}
+        data['ids'] = self_obj.get('active_ids', [])
+        data['model'] = self_obj.get('active_model', 'ir.ui.menu')
+        data['form'] = self.read([
+                        'company_id', 'report_type',
+                        'month_filter', 'year_filter',
+                        'date_filter', 'date_filter_from',
+                        'date_filter_to'])[0]
+        return self._print_data(data)
