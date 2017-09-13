51fe6374e9775607c8f40ccd4feb0a7a668f8824
junifar <junifar@gmail.com>
2017-09-13 11:06:43 +0700

    Import From File Fixed

--- prasetia_hr/wizard/external_data.py
+++ prasetia_hr/wizard/external_data.py
@@ -1,15 +1,16 @@
 import base64
 from datetime import datetime
 
+import pytz
 from odoo.exceptions import ValidationError
 from xlrd import XLRDError
 from xlrd import open_workbook
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 
-
 # import logging
 # _logger = logging.getLogger(__name__)
+from xlrd import xldate_as_tuple
 
 
 class import_external_data(models.TransientModel):
@@ -38,33 +39,136 @@ class import_external_data(models.TransientModel):
             pass
         return value
 
-    def _import_process(self):
+    def utcConvert(self, time_val):
+        local = pytz.timezone(self.env.user.partner_id.tz)
+        local_dt = local.localize(time_val, is_dst=None)
+        return local_dt.astimezone(pytz.utc)
+
+    def _update_check_date(self, ctx, noreg, date_absen, check_in, check_out):
+        hr_employee_absen_book_pool = self.env['hr.employee.absen.book']
+        hr_employee_absen_book_data = hr_employee_absen_book_pool.search([('id', '=', ctx['current_id'])])
+
+        convert_date_absen = lambda x: x.strftime('%Y-%m-%d') if x is not None else None
+
+        for data in hr_employee_absen_book_data:
+            if data.name == convert_date_absen(date_absen):
+                for line in data.absen_ids:
+                    if line.employee_id.registration_id == noreg:
+                        line.absent_in = self.utcConvert(check_in)
+                        line.absent_out = self.utcConvert(check_out)
+                        line.attendance_status = 'hadir'
+        return None
+
+    @staticmethod
+    def _convert_month_to_int(month):
+        months = dict(January=1,
+                      February=2,
+                      March=3,
+                      April=4,
+                      May=5,
+                      June=6,
+                      July=7,
+                      August=8,
+                      September=9,
+                      October=10,
+                      November=11,
+                      December=12)
+        return months[month]
+
+    def _import_process(self, ctx):
         try:
             workbook = open_workbook(file_contents=base64.decodestring(self.bin_file))
             sheet = workbook.sheets()[0]
-            values = []
+            if sheet.cell(3, 2).value is not None:
+                cell_date_absen = sheet.cell(3, 2).value.split()
+                year = int(cell_date_absen[3])
+                month = self._convert_month_to_int(cell_date_absen[2])
+                day = int(cell_date_absen[1])
+                date_absen = datetime(year, month, day)
+            else:
+                date_absen = None
+
+            print '======'
+            print date_absen
+
             for row in range(sheet.nrows):
                 col_values = []
-                if row >= 9:
-                    if not sheet.cell(row, 7).value == '':
-                        value = self._check_str(sheet.cell(row, 1).value)
-                        col_values.append(value)
+                if row >= 5:
+                    if not sheet.cell(row, 6).value == '':
+                        noreg = self._check_str(sheet.cell(row, 2).value)
+                        # try:
+                        #     year, month, day, hour, minute, second = xldate_as_tuple(sheet.cell(row, 3).value, 0)
+                        # except ValueError:
+                        #     pass
+                        #
+                        # try:
+                        #     date_absen = datetime(year, month, day)
+                        # except ValueError:
+                        #     date_absen = None
 
-                        value_hour = self._check_str(sheet.cell(row, 7).value)
-                        value_minute = self._check_str(sheet.cell(row, 8).value)
-                        col_values.append('%s:%s' % (value_hour, value_minute))
+                        value_hour = self._check_str(sheet.cell(row, 6).value)
+                        value_minute = self._check_str(sheet.cell(row, 7).value)
 
-                        value_hour = self._check_str(sheet.cell(row, 9).value)
-                        value_minute = self._check_str(sheet.cell(row, 10).value)
-                        col_values.append('%s:%s' % (value_hour, value_minute))
-                        values.append(col_values)
-                print values
+                        try:
+                            value_in = datetime.strptime(
+                                '%s-%s-%s %s:%s' % (year, month, day, value_hour, value_minute),
+                                '%Y-%m-%d %H:%M')
+                        except TypeError:
+                            value_in = None
+                        except ValueError:
+                            value_in = None
+
+                        value_hour = self._check_str(sheet.cell(row, 8).value)
+                        value_minute = self._check_str(sheet.cell(row, 9).value)
+                        try:
+                            value_out = datetime.strptime(
+                                '%s-%s-%s %s:%s' % (year, month, day, value_hour, value_minute),
+                                '%Y-%m-%d %H:%M')
+                        except TypeError:
+                            value_out = None
+                        except ValueError:
+                            value_out = None
+                        col_values.append(value_out)
+
+                        self._update_check_date(ctx, noreg, date_absen, value_in, value_out)
+
+                        # value = self._check_str(sheet.cell(row, 2).value)
+                        # col_values.append(value)
+
+                        # value = None
+                        # try:
+                        #     year, month, day, hour, minute, second = xldate_as_tuple(sheet.cell(row, 3).value, 0)
+                        # except ValueError:
+                        #     pass
+                        # col_values.append(datetime(year, month, day, hour, minute, second))
+                        #
+                        # value_hour = self._check_str(sheet.cell(row, 6).value)
+                        # value_minute = self._check_str(sheet.cell(row, 7).value)
+                        # try:
+                        #     value_in = datetime.strptime(
+                        #         '%s-%s-%s %s:%s' % (year, month, day, value_hour, value_minute),
+                        #         '%Y-%m-%d %H:%M')
+                        # except ValueError:
+                        #     value_in = None
+                        # col_values.append(value_in)
+                        #
+                        # value_hour = self._check_str(sheet.cell(row, 8).value)
+                        # value_minute = self._check_str(sheet.cell(row, 9).value)
+                        # try:
+                        #     value_out = datetime.strptime(
+                        #         '%s-%s-%s %s:%s' % (year, month, day, value_hour, value_minute),
+                        #         '%Y-%m-%d %H:%M')
+                        # except ValueError:
+                        #     value_out = None
+                        # col_values.append(value_out)
+                        # values.append(col_values)
+                        # print values
         except XLRDError:
             raise ValidationError(_('File Bukan Tipe Excel'))
 
     @api.multi
     def show_data(self):
-        # ctx = self._context
+        ctx = self._context
         # self._insert_sample(ctx)
 
         # try:
@@ -89,5 +193,5 @@ class import_external_data(models.TransientModel):
         # except XLRDError:
         #     raise ValidationError(_('File Bukan Tipe Excell'))
         #     pass
-        self._import_process()
+        self._import_process(ctx)
         return None
